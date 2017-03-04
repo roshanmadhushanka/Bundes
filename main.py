611e55401a213d6146fa39d01e0d052d5a217982
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2017-03-04 15:50:00 +0530

    Image change dynamically

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ company_list_updated = False
 process_queue = ProcessQueue()
 
 # Phantom
-driver = webdriver.PhantomJS(executable_path='C:\Users\Roshan\Downloads\Compressed\phantomjs-2.1.1-windows\\bin\phantomjs.exe')
+# driver = webdriver.PhantomJS(executable_path='C:\Users\Roshan\Downloads\Compressed\phantomjs-2.1.1-windows\\bin\phantomjs.exe')
+driver = webdriver.Firefox(executable_path='C:\Users\Roshan\PycharmProjects\\bundesanzeiger\driver\geckodriver.exe')
 
 
 def updateCompany():
@@ -34,7 +35,7 @@ def updateCompany():
 
 
 def updateProcessQueue():
-    global process_queue, company_list
+    global process_queue, company_list, driver
     if not isinstance(process_queue, ProcessQueue):
         return
 
@@ -45,30 +46,32 @@ def updateProcessQueue():
     process_queue.enqueue(links)
 
 
-@app.route(main_url)
-def index():
-    global company_list
-    if not company_list_updated:
-        updateCompany()
-    session['company_list'] = company_list
-    return render_template('main.html')
-
-
-@app.route('/get_captcha')
-def get_captcha():
+def getCaptcha():
     global process_queue, main_url, driver
     if not isinstance(process_queue, ProcessQueue):
         return
 
     web_url = process_queue.dequeue()
+    if web_url is None:
+        return
 
-    captcha_url = crawler.getCaptchaSource(web_url)
-
-    # captcha_url = crawler.getCaptchaSource(web_url)
+    driver.get(web_url)
+    print web_url
+    captcha_url = crawler.getCaptchaFromPage(driver.page_source)
+    print captcha_url
 
     session['current_url'] = web_url
     session['current_captcha'] = captcha_url
-    return redirect(main_url)
+
+
+@app.route(main_url)
+def index():
+    global company_list
+    if not company_list_updated:
+        updateCompany()
+        session['company_list'] = company_list
+    getCaptcha()
+    return render_template('main.html')
 
 
 @app.route('/send_captcha', methods=['POST'])
@@ -78,8 +81,8 @@ def send_captcha():
         ans_captcha = request.form.get('captcha_solution')
         driver.get(session['current_url'])
 
-        print 'ans_captcha', ans_captcha
-        print 'current_url', session['current_url']
+        # print 'ans_captcha', ans_captcha
+        # print 'current_url', session['current_url']
 
         captcha_input = driver.find_element_by_id("captcha_data.solution")
         captcha_input.send_keys(ans_captcha)
@@ -88,7 +91,7 @@ def send_captcha():
         submit.click()
 
         soup = BeautifulSoup(driver.page_source, 'lxml')
-        print soup.prettify()
+        # print soup.prettify()
 
     return redirect(main_url)
 
--- system/crawler.py
+++ system/crawler.py
@@ -53,3 +53,35 @@ def getCaptchaFromPage(page):
     soup = BeautifulSoup(page, "lxml")
     captcha_result = soup.find_all("img", {"alt": "Captcha"})
     return 'https://www.bundesanzeiger.de/' + captcha_result[0]['src']
+
+
+def getSearchUrlsFromDriver(company_name, driver):
+    '''
+    Search for particular company name and get set of urls related to that company
+    :param company_name: company name that needed to be searched
+    :return: set of urls from the result
+    '''
+
+    driver.get('https://www.bundesanzeiger.de/ebanzwww/wexsservlet')
+
+    captcha_input = driver.find_element_by_id("genericsearch_param.fulltext")
+    captcha_input.send_keys(company_name)
+
+    submit = driver.find_element_by_name("(page.navid=to_quicksearchlist)")
+    submit.click()
+
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    table_result = soup.findAll("table", {"summary": "Trefferliste"})
+    td_results = [a.find_all("td", {"class": "info"}) for a in table_result]
+
+    if len(td_results) == 0:
+        return
+
+    available_links = []
+    for p in td_results:
+        for t in p:
+            for a in t:
+                result_url = 'https://www.bundesanzeiger.de/' + a['href']
+                available_links.append(result_url)
+
+    return available_links
